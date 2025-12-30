e80069384833a09b557c8e9b661b3dde05f1ec12
jaeyeolkim <dev_1@becuai.com>
2025-12-30 14:56:41 +0900

    Initial commit

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,14 @@
+package com.example
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureHTTP()
+    configureRouting()
+}
---
+++ src/main/kotlin/Routing.kt
@@ -0,0 +1,27 @@
+package com.example
+
+import com.asyncapi.kotlinasyncapi.context.service.AsyncApiExtension
+import com.asyncapi.kotlinasyncapi.ktor.AsyncApiPlugin
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        get("/health") {
+            call.respondText("OK")
+        }
+        get("/user/{id}") {
+            // Ktor는 Non-blocking 모델이라 Blocking IO를 분리하지 않으면 전체 성능이 급격히 저하
+            val id = withContext(Dispatchers.IO) {
+                call.parameters["id"]!!.toInt()
+            }
+            call.respondText { "User $id" }
+        }
+    }
+}
